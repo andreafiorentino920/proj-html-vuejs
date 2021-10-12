77ff10025dc6c5ba39d7f72ab37033c200dc4138
andreafiorentino920 <andreafiorentino920@gmail.com>
2021-10-12 20:07:24 +0100

    Meet our trainer section

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,31 @@
 <template>
-  <div>
-      
+  <div class="header-container">
+      <div class="container pt-4 d-flex justify-content-between align-items-center">
+        <div class="logo">
+        </div>
+        <div class="menu">
+          <ul class="d-flex flex-row">
+            <li>
+              <a class="selected" href="#">HOME</a>
+            </li>
+            <li>
+              <a href="#">ABOUT US </a>
+            </li>
+            <li>
+              <a href="#">FACILITIES </a>
+            </li>
+            <li>
+              <a href="#">MEMBERSHIP </a>
+            </li>
+            <li>
+              <a href="#">TESTIMONIALS</a>
+            </li>
+            <li>
+              <a href="#">BLOG</a>
+            </li>
+          </ul>
+        </div>
+      </div>
   </div>
 </template>
 
@@ -10,6 +35,34 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" >
+.header-container{
+  background-image: url("../assets/images/home-banner.jpg");
+  background-size:cover; 
+  height: 800px;
+  .container{
+      .logo{
+      background-image: url("../assets/images/gym_logo_1x.png");
+      width: 250px;
+      height: 100px;
+      }
+      .menu{
+        .selected{
+          color: #ee6059;
+        }
+        ul{
+          li{
+          list-style-type: none;
+          margin-left: 30px;
+          a{
+          color: white;
+          text-decoration: none;
+          }
+          }
+        }
+      }
+  }
+ 
+}
 
 </style>
